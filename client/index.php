ad193f350d52c5cd91e0ab51b2f787fb7a6a22c6
ReasonalWork <reasonalworkplace@gmail.com>
2026-04-29 17:01:32 +0800

    Removed incorrect paths

--- client/index.php
+++ client/index.php
@@ -10,14 +10,14 @@ include_once COMPONENTS . "/index-header.php";
 
 ?>
 
-<img id='orbit-logo' src="<?php echo BASE_URL; ?>/src/resources/icons/orbit-logo.svg" alt="orbit logo" aria-hidden="true">
+<img id='orbit-logo' src="<?php echo ASSETS; ?>/icons/orbit-logo.svg" alt="orbit logo" aria-hidden="true">
         <p class="title">
             Your personal campus navigator and transport manager
         </p>
         <div class="login-button">
             <a href="<?php echo BASE_URL; ?>/src/app/auth/login_page.php">Log In
                 <div class="icon">
-                    <?php include_once $doc_root_folder . "/src/resources/icons/login-square-icon.svg" ?>
+                    <?php include_once ASSETS . "/icons/login-square-icon.svg" ?>
                 </div>
             </a>
         </div>
