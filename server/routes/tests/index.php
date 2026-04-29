af54dae20f025bbcfdcd6cb25cb0d0282e23c104
ReasonalWork <reasonalworkplace@gmail.com>
2026-04-29 18:41:15 +0800

    Moved and renamed nav-bar-test

--- server/routes/tests/index.php
+++ server/routes/tests/index.php
@@ -2,27 +2,19 @@
 /**
  * Entry point for testing.
 */
-
-session_start();
 require $_SERVER['DOCUMENT_ROOT'] . '/Orbit/shared/constants.php';
-
-// include ROOT.CONFIG;
-
-include_once ROOT.COMPONENTS . "/header.php";
-include_once ROOT.COMPONENTS . "/footer.php";
-include_once ROOT.COMPONENTS . "/nav-bar.php";
-
-$currentPage = 'home';
-$_SESSION['currentPage'] = $currentPage; // Set the current page for active state testing
-$userRole = 'student'; // Change this to test different roles
-$_SESSION['user_role'] = $userRole; // Set the user role in session for testing
-
-createHead('Test Page', ['global.css', 'test-page.css', 'nav-bar.css']);
-renderNavBar();
+include ROOT.CONFIG;
+require_once ROOT.CONTROLLERS.'/manage-user.php';
 ?>
 
-<script src="<?php echo FEATURES . '/ripple-effect.js'; ?>"></script>
+<html>
+<head>
+    <title>Tests</title>
+</head>
 
-<?php 
-createFooter(false);
-?>
+<body>
+    <h1>Tests</h1>
+    <p><?php
+    ?></p>
+</body>
+</html>
