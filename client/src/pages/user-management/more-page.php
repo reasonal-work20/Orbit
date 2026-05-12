36b85edd7b8f91fb5027f4da4c1da1eb39be8f75
Cheyenne <cheyennechoong@gmail.com>
2026-05-12 08:06:25 +0800

    Integration

--- client/src/pages/user-management/more-page.php
+++ client/src/pages/user-management/more-page.php
@@ -4,8 +4,7 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/Orbit/shared/constants.php';
 
 include_once ROOT . COMPONENTS . '/header.php';
 include_once ROOT . COMPONENTS . '/footer.php';
-require_once ROOT . COMPONENTS . '/admin-nav-bar.php';
-require_once ROOT . CONFIG; // Establish connection to database and start session
+require_once ROOT . NAVIGATION . '/admin-nav-bar.php';
 require_once ROOT . SERVICES . '/manage-user-service.php';
 require_once ROOT . MODALS . '/modal.php';
 
@@ -25,11 +24,6 @@ $_SESSION['currentPage'] = 'more';
 $cssFiles = ["admin-dashboard.css", "admin-top-bar.css", 'admin-nav-bar.css'];
 
 createHead("Orbit | More", $cssFiles);
-
-// Profile modal (hidden by default)
-$modalObj = new Modal('userProfileView', 'large');
-$profileHtml = $modalObj->formatUserProfile($user);
-echo $modalObj->render("My Profile", $profileHtml);
 
 // Nav Bar and content top bar.
 renderNavBar();
