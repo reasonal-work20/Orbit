939e38470eefe8f72764b9a5fbc275937a630052
Cheyenne <cheyennechoong@gmail.com>
2026-05-20 13:10:48 +0800

    Update

---
+++ server/models/schedule.php
@@ -0,0 +1,16 @@
+<?php
+
+class Schedule {
+    private $connection;
+
+    public function __construct($database) {
+        $this->connection = $database;
+    }
+
+    public function create() {}
+
+    public function update() {}
+
+    public function delete() {}
+}
+?>
